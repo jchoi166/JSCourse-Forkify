34144b44a713488796dcd59c63f79a5d79660fd9
Spike <jchoi166@gmail.com>
2020-10-05 08:01:31 -0700

    built shopping list model and view

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import Search from './models/Search'
+import Recipe from './models/Recipe'
+import List from './models/List'
+import Likes from './models/Likes'
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
+import * as listView from './views/listView'
 import { elements, renderLoader, clearLoader } from './views/base'
-import Recipe from './models/Recipe'
 /**Global state of the app
  * - Search object
  * - Current recipe object
@@ -11,8 +14,8 @@ import Recipe from './models/Recipe'
  */
 
 const state = {
-
 }
+window.state = state
 
 
 // SEARCH CONTROLLER
@@ -96,11 +99,70 @@ const controlRecipe = async () => {
     }
 }
 
-
-// window.addEventListener('hashchange', controlRecipe)
-// window.addEventListener('load', controlRecipe)
-
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe))
+
+// List Controller
+
+const controlList = () => {
+    // Create a new list IF there is none yet
+    if (!state.list) state.list = new List()
+
+    // Add each ingredient to the list
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient)
+        listView.renderItem(item)
+    })
+}
+
+// Handle delete and update item events 
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid
+    // console.log(id)
+    // Handle the delete 
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id)
+
+        // Delete from UI
+        listView.deleteItem(id)
+
+    // Handle the count update
+    } else if (e.target.matches('.shopping__count-value')){
+        const val = parseFloat(e.target.value, 10)
+        state.list.updateCount(id, val)
+    }
+})
+
+// Like Controller
+const controlLike = () => {
+    if(!state.likes) state.likes = new Likes() 
+    const currentID = state.recipe.id
+    console.log(state.likes)
+
+    // User has NOT yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        // Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        )
+        // Toggle the like button
+
+        // Add like to the UI list
+        console.log(state.likes)
+
+    // User HAS liked current recipe
+    } else {
+        // Remove like from the state
+        state.likes.deleteLike(currentID)
+        // Toggle the like button
+
+        // Remove like from UI list
+        console.log(state.likes)
+    }
+}
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
@@ -114,6 +176,13 @@ elements.recipe.addEventListener('click', e => {
         // Increase button is clicked
         state.recipe.updateServings('inc')
         recipeView.updateServingsIngredients(state.recipe)
+    } else if (e.target.matches ('.recipe__btn--add, .recipe__btn--add *')) {
+        // Add ingredients to shopping list
+        controlList()
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // Like controller 
+        controlLike()
     }
-    console.log(state.recipe)
 })
+
+window.l = new List()
